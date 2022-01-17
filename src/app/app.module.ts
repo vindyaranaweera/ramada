e6c1c9294e36e88cf9f08f4cd3a1b27b46401ae6
unknown <vindyayounghwa262@gmail.com>
2022-01-17 10:38:44 +0530

    time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
+import {DatePipe, registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -84,7 +84,7 @@ registerLocaleData(en);
         NzRateModule,
         NzSwitchModule
     ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [{ provide: NZ_I18N, useValue: en_US },DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule {
