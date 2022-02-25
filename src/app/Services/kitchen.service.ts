dfb30208a83732f5a753ec3ccbff3e9c1809a853
vindyaranaweera <vindyayounghwa262@gmail.com>
2022-02-25 14:55:10 +0530

    pulling new updates

--- src/app/Services/kitchen.service.ts
+++ src/app/Services/kitchen.service.ts
@@ -8,6 +8,7 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 export class KitchenService {
 
   private apiServerUrl='http://143.198.36.89/api/';
+  // private apiServerUrl='http://localhost:8080';
 
   constructor(private http:HttpClient) { }
 
